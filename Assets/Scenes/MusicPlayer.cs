f85fca60ac14e3fd60170612973d4587de2380d0
Simon <simon.wardle@outlook.com>
2019-01-31 23:30:14 +0000

    Added Splash screen, music and a circuit

---
+++ Assets/Scenes/MusicPlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadNextLevel", 5f);
+    }
+
+    // Update is called once per frame
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
